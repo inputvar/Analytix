b5e9f637a1716abcf4e51a4995e6e144f1a45da0
VirajSindhwad <virajsindhwadd@gmail.com>
2023-10-29 03:27:07 +0530

    further frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header';
-import Feedback from './components/Feedback'
+import Feedback from './components/Feedback';
+import {useState} from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
@@ -8,20 +9,28 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Admin from './pages/Admin';
+
+import About from './components/About';
+import Contact from './components/Contact';
+
 
 function App() {
+
+
   return (
     <BrowserRouter>
-      <Header />
+    <Header/>
       <br></br>
       <Routes>
+      <Route path="/admin" element={<Admin />}/> 
       <Route path="/" element={<Home />} />      
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/feedback" element={<Feedback />} />
 
-      <Route path="/about" element={<Feedback />} />
-      <Route path="/contact" element={<Feedback />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
 
         {/* <Route path="/" element={<Home />} /> */}
       </Routes>
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import GoogleButton from 'react-google-button';
+
 
 function Signup() {
   return (
-    <div>Signup</div>
+    <main className="form-signin w-100 m-auto mt-5">
+  <form>
+    <h1 className="h3 mb-3 fw-normal">Please Sign Up</h1>
+
+    <div className="form-floating mt-4">
+      <input type="text" className="form-control" id="floatingInput" placeholder="name@example.com" required/>
+      <label for="floatingInput">Username</label>
+    </div>                                                                                                                                                    
+
+    <div className="form-floating mt-4">
+      <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" required/>
+      <label for="floatingInput">Email address</label>
+    </div>
+    <div className="form-floating mt-4">
+      <input type="password" className="form-control" id="floatingPassword" placeholder="Password" required/>
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    {/* <div className="form-check text-start my-3">
+      <input className="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault"/>
+      <label className="form-check-label" for="flexCheckDefault">
+        Remember me
+      </label>
+    </div> */}
+    <button className="btn btn-dark w-100 py-2 mt-5" type="submit">Sign up</button>
+    <GoogleButton type="dark" className='mt-3 w-100' style={{"border-radius" : "3px"}}/>
+
+  </form>
+</main>
   )
 }
 
